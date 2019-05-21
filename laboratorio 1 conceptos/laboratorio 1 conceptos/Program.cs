3aeec90a19bb27fa9837338a9f78ebcb01aa89ae
Daniel <dsestupinan71@misena.edu.co>
2019-05-20 22:02:54 -0500

    Initial commit

---
+++ laboratorio 1 conceptos/laboratorio 1 conceptos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace laboratorio_1_conceptos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
